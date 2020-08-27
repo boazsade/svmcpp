4e24cf1671f2d5175e08be2bcb8ccff2259fc580
Boaz Sade <boaz.sade@binah.ai>
2020-08-27 06:17:11 +0300

    update hte dot product file

--- src/vector/include/dot_product.hpp
+++ src/vector/include/dot_product.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "vector.hpp"
 #include <numeric>
+#include <functional>
 
 namespace svm
 {
@@ -8,13 +9,79 @@ namespace svm
 template<typename Iterator>
 inline constexpr typename Iterator::value_type 
     dot_product(Iterator start1, Iterator end1, Iterator start2) noexcept {
-        return std::transform_reduce(start1, end1, start2, std::multiplies<typename Iterator::value_type>{});
+        using value_type = typename Iterator::value_type;
+
+        return std::transform_reduce(start1, end1, start2, value_type{},
+                std::plus<value_type>{}, 
+                std::multiplies<value_type>{});
 }
 
 template<typename Iterator, typename ExecutionPolicy>
 inline constexpr typename Iterator::value_type 
     dot_product(ExecutionPolicy&& policy, Iterator start1, Iterator end1, Iterator start2) noexcept {
-        return std::transform_reduce(policy, start1, end1, start2, std::multiplies<typename Iterator::value_type>{});
+        using value_type = typename Iterator::value_type;
+
+        return std::transform_reduce(policy, start1, end1, start2, value_type{},
+                std::plus<value_type>{},
+                std::multiplies<value_type>{}
+            );
+}
+
+inline constexpr dynamic_vector::value_type 
+    dot_product(const dynamic_vector& left, const dynamic_vector& right) noexcept {
+        if (std::size(vec) != std::size(left)) {
+            return invalid();
+        } else {
+            return dot_product(std::begin(left), std::end(left), std::begin(right));
+        }
+}
+
+inline constexpr dynamic_vector::value_type 
+    dot_product(const static_vector& left, const dynamic_vector& right) noexcept {
+        if (std::size(vec) != std::size(left)) {
+            return invalid();
+        } else {
+            return dot_product(std::begin(left), std::end(left), std::begin(right));
+        }
+}
+
+inline constexpr dynamic_vector::value_type 
+    dot_product(const dynamic_vector& left, const static_vector& right) noexcept {
+        if (std::size(vec) != std::size(left)) {
+            return invalid();
+        } else {
+            return dot_product(std::begin(left), std::end(left), std::begin(right));
+        }
+}
+
+template<typename ExecutionPolicy>
+inline constexpr dynamic_vector::value_type 
+    dot_product(ExecutionPolicy&& ep, const dynamic_vector& left, const dynamic_vector& right) noexcept {
+        if (std::size(vec) != std::size(left)) {
+            return invalid();
+        } else {
+            return dot_product(std::move(ep), std::begin(left), std::end(left), std::begin(right));
+        }
+}
+
+template<typename ExecutionPolicy>
+inline constexpr dynamic_vector::value_type 
+    dot_product(const static_vector& left, const dynamic_vector& right) noexcept {
+        if (std::size(vec) != std::size(left)) {
+            return invalid();
+        } else {
+            return dot_product(std::move(ep), std::begin(left), std::end(left), std::begin(right));
+        }
+}
+
+template<typename ExecutionPolicy>
+inline constexpr dynamic_vector::value_type 
+    dot_product(ExecutionPolicy&& ep, const dynamic_vector& left, const static_vector& right) noexcept {
+        if (std::size(vec) != std::size(left)) {
+            return invalid();
+        } else {
+            return dot_product(std::move(ep), std::begin(left), std::end(left), std::begin(right));
+        }
 }
 
 }   // end of namespace svm
